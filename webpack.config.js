18ba27151a56b0419e4201796ecf7e6c41cfe9d0
BonBon <bonbon.chen@basebit.ai>
2021-12-12 23:41:51 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
 const path = require("path");
-const webpack = require("webpack");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.(js|ts|jsx|tsx)$/,
+        exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: ["*", ".js", "ts", ".jsx", "tsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    hot: true,
+    open: true,
+    historyApiFallback: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
